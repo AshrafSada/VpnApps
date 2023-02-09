49913e29c4be8668c866dcb63ee25d237bdbb9b7
Ashraf Sada <ashraf.sada@outlook.com>
2023-02-09 19:08:43 +0300

    - Added new project Basic VPN

---
+++ VpnApps/VpnApps.BasicVpn/Program.cs
@@ -0,0 +1,23 @@
+﻿using VpnApps.BasicVpn;
+using static System.Console;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        WriteLine("Basic VPN App");
+
+        WriteLine("Connect VPN [y]/[N]?");
+
+        char result = Convert.ToChar(ReadLine());
+
+        if (result == 'y' || result == 'Y')
+        {
+            RasManager.ConnectVpn();
+        }
+        else
+        {
+            WriteLine("OK got it!");
+        }
+    }
+}
---
+++ VpnApps/VpnApps.BasicVpn/RasManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace VpnApps.BasicVpn;
+
+internal class RasManager
+{
+    private const string SERVER_ADDRESS = "us2.vpnbook.com";
+    private const string USER_NAME = "vpnbook";
+    private const string USER_PWD = "xkud5hn";
+
+    public static void ConnectVpn()
+    {
+        if (CreateRasphoneFile())
+        {
+            // TODO: connect to server
+        }
+    }
+
+    private static bool CreateRasphoneFile()
+    {
+        string userProfilePath = Environment.GetFolderPath(
+           Environment.SpecialFolder.UserProfile);
+        string rasFolder = @"AppData\Roaming\Microsoft\Network\Connections\Pbk";
+        string pbkFile = Path.Combine(userProfilePath, rasFolder, "rasphone.pbk ");
+
+        var sb = new StringBuilder();
+        sb.AppendLine("[MyVPNServer]");
+        sb.AppendLine("MEDIA=rastapi");
+        sb.AppendLine("Port=VPN2-0");
+        sb.AppendLine("Device=WAN Miniport (IKEv2)");
+        sb.AppendLine("DEVICE=vpn");
+        sb.AppendLine($"PhoneNumber={SERVER_ADDRESS}");
+
+        File.WriteAllText(pbkFile, sb.ToString(), Encoding.UTF8);
+
+        return File.Exists(pbkFile);
+    }
+}
